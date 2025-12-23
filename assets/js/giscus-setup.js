dd058682ed9a7592c27c4626dea5bacdd216aa48
asarkar1 <asarkar1@users.noreply.github.com>
2025-12-23 06:10:37 +0000

    Deploying to gh-pages from @ asarkar1/asarkar1.github.io@6421541c0f4fb11015501bae48141fee2f0d051d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "asarkar1/asarkar1.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
